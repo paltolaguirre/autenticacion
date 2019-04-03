73e3e8523c71a9e14209382eea177a08f4eea7cb
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-04-03 11:30:15 -0300

    cambio en la respuesta, ahora devuelve un json

--- actions.go
+++ actions.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"fmt"
-    "net/http"
-   // "log"
-  //  "github.com/gorilla/mux"
-    "encoding/json"
-  	//"github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
-  	"time"
- //   "github.com/xubiosueldos/conexionBD"
-  	"github.com/xubiosueldos/autenticacion/publico"
+	"net/http"
+
+	// "log"
+	//  "github.com/gorilla/mux"
+	"encoding/json"
+	//"github.com/jinzhu/gorm"
+	"time"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	//   "github.com/xubiosueldos/conexionBD"
+	"github.com/xubiosueldos/autenticacion/publico"
 )
-
-
-
-
 
 //var db *gorm.DB
 var err error
-var m = make(map[string] publico.TokenAutenticacion)
+var m = make(map[string]publico.TokenAutenticacion)
 var errors publico.Error
 
-func Login(w http.ResponseWriter, r *http.Request){
+func Login(w http.ResponseWriter, r *http.Request) {
 
 	username := r.FormValue("username")
 	pass := r.FormValue("pass")
@@ -39,13 +38,12 @@ func Login(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application-json")
 	w.WriteHeader(200)
 
-	json.NewEncoder(w).Encode(token)
+	json.NewEncoder(w).Encode(autenticacion)
 	fmt.Println(m[string(token)])
 	fmt.Println(autenticacion)
 }
 
-
-func CheckToken(w http.ResponseWriter, r *http.Request){
+func CheckToken(w http.ResponseWriter, r *http.Request) {
 
 	header := r.Header
 
@@ -53,15 +51,14 @@ func CheckToken(w http.ResponseWriter, r *http.Request){
 
 	fmt.Println(m[token])
 
-	tokenAutenticacion,ok := m[token]
+	tokenAutenticacion, ok := m[token]
 
-	if(ok){
+	if ok {
 		w.Header().Set("Content-Type", "application-json")
 		w.WriteHeader(200)
 
 		json.NewEncoder(w).Encode(tokenAutenticacion)
-	} else 
-	{
+	} else {
 		errors = publico.Error{ErrorNombre: "Hubo error", ErrorCodigo: 400}
 
 		w.Header().Set("Content-Type", "application-json")
@@ -70,5 +67,3 @@ func CheckToken(w http.ResponseWriter, r *http.Request){
 		json.NewEncoder(w).Encode(errors)
 	}
 }
-
-
