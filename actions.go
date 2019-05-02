0ac2bceb4d824823d0a01488ac0186c06b8f1f9c
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-02 14:27:32 -0300

    encode/decode de token

--- actions.go
+++ actions.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"encoding/base64"
+	"math/rand"
 	"net/http"
+	"strconv"
+	s "strings"
 	"time"
 
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/xubiosueldos/autenticacion/publico"
+	"github.com/xubiosueldos/framework"
 )
 
 //var db *gorm.DB
@@ -17,11 +20,26 @@ var errors publico.Error
 
 func Login(w http.ResponseWriter, r *http.Request) {
 
-	username := r.FormValue("username")
-	pass := r.FormValue("pass")
-	tenant := r.FormValue("tenant")
+	header := r.Header.Get("Authorization")
+	tokenEncode := s.Split(header, " ")[1]
 
-	token := time.Now().Format("2006-01-02 15:04:05.000000")
+	tokenDecode, err := base64.StdEncoding.DecodeString(tokenEncode)
+
+	if err != nil {
+		framework.RespondError(w, http.StatusNotFound, err.Error())
+		return
+	}
+	numeroRandom := rand.Int63()
+
+	token := strconv.FormatInt(time.Now().UnixNano(), 10) + strconv.FormatInt(numeroRandom, 10)
+
+	//token := time.Now().Format("2006-01-02 15:04:05.000000")
+	infoUser := s.Split(string(tokenDecode), ":")
+
+	username := infoUser[0]
+	pass := infoUser[1]
+	tenant := infoUser[2]
+
 	fecha := time.Now()
 
 	autenticacion := publico.TokenAutenticacion{Username: username, Pass: pass, Tenant: tenant, Token: token, FechaCreacion: fecha}
@@ -29,35 +47,24 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	//VER COMO PREGUNTAR SI EL TOKEN YA ESTA INGRESADO EN EL HASHMAP
 	m[string(token)] = autenticacion
 
-	w.Header().Set("Content-Type", "application-json")
-	w.WriteHeader(200)
+	framework.RespondJSON(w, 200, autenticacion)
 
-	json.NewEncoder(w).Encode(autenticacion)
-	fmt.Println(m[string(token)])
-	fmt.Println(autenticacion)
 }
 
 func CheckToken(w http.ResponseWriter, r *http.Request) {
 
-	header := r.Header
+	header := r.Header.Get("Authorization")
 
-	token := header.Get("Token")
-
-	fmt.Println(m[token])
+	token := s.Split(header, " ")[1]
 
 	tokenAutenticacion, ok := m[token]
 
 	if ok {
-		w.Header().Set("Content-Type", "application-json")
-		w.WriteHeader(200)
+		framework.RespondJSON(w, http.StatusOK, tokenAutenticacion)
 
-		json.NewEncoder(w).Encode(tokenAutenticacion)
 	} else {
-		errors = publico.Error{ErrorNombre: "Hubo error", ErrorCodigo: 400}
+		errors = publico.Error{ErrorNombre: "Hubo error", ErrorCodigo: http.StatusNotFound}
+		framework.RespondError(w, errors.ErrorCodigo, errors.ErrorNombre)
 
-		w.Header().Set("Content-Type", "application-json")
-		w.WriteHeader(errors.ErrorCodigo)
-
-		json.NewEncoder(w).Encode(errors)
 	}
 }
