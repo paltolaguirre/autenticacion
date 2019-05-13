742eca138959e1dc00ced842cfb177fcbdc5c17e
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-13 11:38:49 -0300

    nuevos metodos para buscar en la bd el token

--- actions.go
+++ actions.go
@@ -8,14 +8,17 @@ import (
 	s "strings"
 	"time"
 
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/xubiosueldos/autenticacion/publico"
+	"github.com/xubiosueldos/conexionBD"
 	"github.com/xubiosueldos/framework"
+	"github.com/xubiosueldos/framework/configuracion"
 )
 
 //var db *gorm.DB
 var err error
-var m = make(map[string]publico.TokenAutenticacion)
+var m = make(map[string]publico.Security)
 var errors publico.Error
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -34,22 +37,10 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		}
 
 		//token := time.Now().Format("2006-01-02 15:04:05.000000")
-		infoUser := s.Split(string(tokenDecode), ":")
 
-		username := infoUser[0]
-		pass := infoUser[1]
-		tenant := infoUser[2]
+		security := insertarTokenSecurity(tokenDecode, w)
 
-		numeroRandom := rand.Int63()
-		token := strconv.FormatInt(time.Now().UnixNano(), 10) + strconv.FormatInt(numeroRandom, 10)
-
-		fecha := time.Now()
-
-		autenticacion := publico.TokenAutenticacion{Username: username, Pass: pass, Tenant: tenant, Token: token, FechaCreacion: fecha}
-
-		m[string(token)] = autenticacion
-
-		framework.RespondJSON(w, http.StatusOK, autenticacion)
+		framework.RespondJSON(w, http.StatusOK, security)
 
 	} else {
 		framework.RespondError(w, http.StatusNotFound, framework.DatosIncorrectos)
@@ -64,23 +55,24 @@ func CheckToken(w http.ResponseWriter, r *http.Request) {
 
 	token := s.Split(header, " ")[1]
 
-	tokenAutenticacion, ok := m[token]
+	security, ok, err := checkTokenDB(w, token)
 
 	if ok {
-		framework.RespondJSON(w, http.StatusOK, tokenAutenticacion)
+		framework.RespondJSON(w, http.StatusOK, security)
 
 	} else {
-		errors = publico.Error{ErrorNombre: "Hubo error", ErrorCodigo: http.StatusUnauthorized}
-		framework.RespondError(w, errors.ErrorCodigo, errors.ErrorNombre)
+		//errors = publico.Error{ErrorNombre: "Hubo error", ErrorCodigo: http.StatusUnauthorized}
+		framework.RespondError(w, http.StatusUnauthorized, err.Error())
 		return
 	}
 }
 
 func chequeoAuthenticationMonolitico(tokenEncode string, r *http.Request) bool {
 
+	configuracion := configuracion.GetInstance()
 	infoUserValida := false
 
-	url := "http://localhost:8080/NXV/SecurityAuthenticationGo"
+	url := configuracion.Url + "SecurityAuthenticationGo"
 
 	req, _ := http.NewRequest("GET", url, nil)
 
@@ -95,4 +87,44 @@ func chequeoAuthenticationMonolitico(tokenEncode string, r *http.Request) bool {
 	}
 
 	return infoUserValida
+}
+
+func insertarTokenSecurity(tokenDecode []byte, w http.ResponseWriter) publico.Security {
+
+	db := conexionBD.ConnectBD("security")
+	defer db.Close()
+
+	infoUser := s.Split(string(tokenDecode), ":")
+
+	username := infoUser[0]
+	pass := infoUser[1]
+	tenant := infoUser[2]
+
+	numeroRandom := rand.Int63()
+	token := strconv.FormatInt(time.Now().UnixNano(), 10) + strconv.FormatInt(numeroRandom, 10)
+
+	fecha := time.Now()
+
+	security := publico.Security{Username: username, Pass: pass, Tenant: tenant, Token: token, FechaCreacion: fecha}
+
+	if err := db.Create(&security).Error; err != nil {
+		framework.RespondError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	return security
+}
+
+func checkTokenDB(w http.ResponseWriter, token string) (publico.Security, bool, error) {
+
+	var existeToken bool = true
+	var security publico.Security
+	var err error = nil
+	db := conexionBD.ConnectBD("security")
+
+	if err = db.Set("gorm:auto_preload", true).First(&security, "token = ?", token).Error; gorm.IsRecordNotFoundError(err) {
+		framework.RespondError(w, http.StatusNotFound, err.Error())
+		existeToken = false
+	}
+
+	return security, existeToken, err
 }
